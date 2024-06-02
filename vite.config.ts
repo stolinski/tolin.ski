074a6942bf21f46e90fb25d0672db05dcba04338
Scott Tolinski <scott.tolinski@gmail.com>
2024-06-02 11:05:40 -0600

    sets to defaults

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [
-		nodePolyfills({
-			// To exclude specific polyfills, add them to this list.
-			// exclude: [
-			// 	'fs', // Excludes the polyfill for `fs` and `node:fs`.
-			// ],
-			// Whether to polyfill specific globals.
-			globals: {
-				// path: true,
-				// Buffer: true, // can also be 'build', 'dev', or false
-				// global: true,
-				// process: true,
-			},
-			// Whether to polyfill `node:` protocol imports.
-			protocolImports: true,
-		}),
-		sveltekit(),
-	],
+	plugins: [nodePolyfills(), sveltekit()],
 	server: {
 		fs: {
 			strict: false,
