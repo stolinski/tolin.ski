6c5cc6e3b59c08cec61cf33d29fd503ca9f595b3
Scott Tolinski <scott.tolinski@gmail.com>
2024-06-02 11:05:05 -0600

    adds node polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,27 @@
 // vite.config.js
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+		nodePolyfills({
+			// To exclude specific polyfills, add them to this list.
+			// exclude: [
+			// 	'fs', // Excludes the polyfill for `fs` and `node:fs`.
+			// ],
+			// Whether to polyfill specific globals.
+			globals: {
+				// path: true,
+				// Buffer: true, // can also be 'build', 'dev', or false
+				// global: true,
+				// process: true,
+			},
+			// Whether to polyfill `node:` protocol imports.
+			protocolImports: true,
+		}),
+		sveltekit(),
+	],
 	server: {
 		fs: {
 			strict: false,
